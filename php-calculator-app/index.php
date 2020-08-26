10a7c0209ab5338b704b32ace8b1b841dc6b4742
Ajinkya G <ajinkyacodes@gmail.com>
2020-08-27 03:24:32 +0530

    Added favicon

--- php-calculator-app/index.php
+++ php-calculator-app/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Calculator</title>
+    <link rel="icon" href="calc-favicon.png" type="image/gif" sizes="16x16">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
