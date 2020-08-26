da270341d87aeb17c07ccb6b7f96e6441d645024
Ajinkya G <ajinkyacodes@gmail.com>
2020-08-27 02:43:35 +0530

    basic styling completed for calculator

--- php-calculator-app/index.php
+++ php-calculator-app/index.php
@@ -7,25 +7,26 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="container">
-        <div class="title">
-            <p>PHP CALCULATOR</p>
-        </div>
-        <div class="calculator">
-            <form name="php-calc-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-                <input type="number" name="num1" id="num1">
-                <input type="number" name="num2" id="num2">
-                <select name="operation" id="operation">
-                    <option value="add">ADD</option>
-                    <option value="sub">SUB</option>
-                    <option value="mul">MULT</option>
-                    <option value="div">DIV</option>
-                </select>
-                <button type="submit" name="calc-submit">SUBMIT</button>
-            </form>
+    <main>
+        <div class="container">
+            <div class="title">
+                <p class="blink_me">PHP CALCULATOR</p>
+            </div>
+            <div class="calculator">
+                <form name="php-calc-form" class="php-calc-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                    <input type="number" name="num1" id="num1" placeholder="Number 1">
+                    <input type="number" name="num2" id="num2" placeholder="Number 2">
+                    <select name="operation" id="operation">
+                        <option value="add">ADD</option>
+                        <option value="sub">SUB</option>
+                        <option value="mul">MULT</option>
+                        <option value="div">DIV</option>
+                    </select>
+                    <button type="submit" name="calc-submit">SUBMIT</button>
+                </form>
+            </div>
         </div>
         <div class="display">
-            <p>
             <?php
                 // define variables and set to empty values
                 $num1 = $num2 = "";
@@ -40,19 +41,19 @@
 
                             switch($operation) {
                                 case "add": $sum = $num1 + $num2;
-                                            echo "The addition of two numbers is {$sum}";
+                                            echo "<span class='type'>ADDITION</span> <p>{$num1} + {$num2} = {$sum}</p>";
                                             break;
 
                                 case "sub": $sub = $num1 - $num2;
-                                            echo "The subtraction of two numbers is {$sub}";
+                                            echo "<span class='type'>SUBTRACTION</span> <p>{$num1} - {$num2} = {$sub}</p>";
                                             break;
 
                                 case "mul": $mul = $num1 * $num2;
-                                            echo "The multiplication of two numbers is {$mul}";
+                                            echo "<span class='type'>MULTIPLICATION</span> <p>{$num1} x {$num2} = {$mul}</p>";
                                             break;
 
                                 case "div": $div = $num1 / $num2;
-                                            echo "The division of two numbers is {$div}";
+                                            echo "<span class='type'>DIVISION</span> <p>{$num1} / {$num2} = {$div}</p>";
                                             break;
 
                                 default: echo "Sorry. It doesn't exist.";
@@ -67,8 +68,7 @@
                     return $data;
                 }
             ?>
-            </p>
         </div>
-    </div>
+    </main>
 </body>
 </html>
