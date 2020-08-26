ee290b9de9a555c1b4ca3f14f9fb90c4bf01a8a5
Ajinkya G <ajinkyacodes@gmail.com>
2020-08-26 22:53:14 +0530

    created basic html code for UI output.

--- php-calculator-app/index.php
+++ php-calculator-app/index.php
@@ -6,7 +6,24 @@
     <title>PHP Calculator</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    <p>PHP CALCULATOR</p>
+<body>    
+    <div class="container">
+        <div class="title">
+            <p>PHP CALCULATOR</p>
+        </div>
+        <div class="calculator">
+            <form name="php-calc-form" action="calc-operations.php">
+                <input type="text" name="firstno">
+                <input type="text" name="secondno">
+                <select name="operations" id="operations">
+                    <option value="add">ADD</option>
+                    <option value="add">SUB</option>
+                    <option value="add">MULT</option>
+                    <option value="add">DIV</option>                    
+                </select>
+                <button type="submit">SUBMIT</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
