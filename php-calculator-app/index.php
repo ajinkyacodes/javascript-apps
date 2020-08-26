c9d3e8585d239a18d1ebd3be5457e04d85ab3897
Ajinkya G <ajinkyacodes@gmail.com>
2020-08-27 01:04:45 +0530

    PHP basic operations functionality added

--- php-calculator-app/index.php
+++ php-calculator-app/index.php
@@ -6,23 +6,68 @@
     <title>PHP Calculator</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>    
+<body>
     <div class="container">
         <div class="title">
             <p>PHP CALCULATOR</p>
         </div>
         <div class="calculator">
-            <form name="php-calc-form" action="calc-operations.php">
-                <input type="text" name="firstno">
-                <input type="text" name="secondno">
-                <select name="operations" id="operations">
+            <form name="php-calc-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                <input type="number" name="num1" id="num1">
+                <input type="number" name="num2" id="num2">
+                <select name="operation" id="operation">
                     <option value="add">ADD</option>
-                    <option value="add">SUB</option>
-                    <option value="add">MULT</option>
-                    <option value="add">DIV</option>                    
+                    <option value="sub">SUB</option>
+                    <option value="mul">MULT</option>
+                    <option value="div">DIV</option>
                 </select>
-                <button type="submit">SUBMIT</button>
+                <button type="submit" name="calc-submit">SUBMIT</button>
             </form>
+        </div>
+        <div class="display">
+            <p>
+            <?php
+                // define variables and set to empty values
+                $num1 = $num2 = "";
+
+                if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    if(isset($_POST['calc-submit'])){                   
+                        $num1 = test_input($_POST["num1"]);
+                        $num2 = test_input($_POST["num2"]);
+
+                        if((isset($num1)&&!empty($num1))&&(isset($num2)&&!empty($num2))) {
+                            $operation = $_POST['operation'];
+
+                            switch($operation) {
+                                case "add": $sum = $num1 + $num2;
+                                            echo "The addition of two numbers is {$sum}";
+                                            break;
+
+                                case "sub": $sub = $num1 - $num2;
+                                            echo "The subtraction of two numbers is {$sub}";
+                                            break;
+
+                                case "mul": $mul = $num1 * $num2;
+                                            echo "The multiplication of two numbers is {$mul}";
+                                            break;
+
+                                case "div": $div = $num1 / $num2;
+                                            echo "The division of two numbers is {$div}";
+                                            break;
+
+                                default: echo "Sorry. It doesn't exist.";
+                            }
+                        }
+                    }
+                }
+                function test_input($data) {
+                    $data = trim($data);
+                    $data = stripslashes($data);
+                    $data = htmlspecialchars($data);
+                    return $data;
+                }
+            ?>
+            </p>
         </div>
     </div>
 </body>
