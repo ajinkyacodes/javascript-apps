1e88b5d79647b03d271bb9fea9dd707025ef7f5b
ajinkyacodes <ajinkyacodes@gmail.com>
2023-06-28 13:06:23 +0530

    password generator app created

--- password-generator/assets/scripts/app.js
+++ password-generator/assets/scripts/app.js
@@ -1,10 +1,63 @@
 //MDN Reference (fromCharCode): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password) { return}
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert("Password copied to clipboard");
+});
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasUpper = uppercaseEl.checked;
+    const hasLower = lowercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+    
+    resultEl.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(upper, lower,number,symbol,length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{upper}, {lower}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    if(typesCount === 0) {
+        return '';
+    }
+
+    for(let i=0; i<length; i+=typesCount) {
+        typesArr.forEach(type=> {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
 }
 
 function getRandomLower() {
