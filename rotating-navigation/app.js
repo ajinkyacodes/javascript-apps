8f8724bcd5dd6bd0ae7a8a21a63a3d91b1894ad0
AJINKYA <ajinkyacodes@gmail.com>
2023-07-07 08:50:51 +0530

    code optimised

--- rotating-navigation/app.js
+++ rotating-navigation/app.js
@@ -1,7 +1,16 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
+const body = document.querySelector('body');
 
-open.addEventListener('click', () => container.classList.add('show-nav'));
-close.addEventListener('click', () => container.classList.remove('show-nav'));
+open.addEventListener('click', () => {
+    body.style.overflow = 'hidden';
+    container.classList.add('show-nav');
+});
+
+
+close.addEventListener('click', () => {
+    body.style.overflow = 'auto';
+    container.classList.remove('show-nav');
+});
 
