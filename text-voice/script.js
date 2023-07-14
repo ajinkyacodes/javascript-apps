446644c93f326a0937d567602260f853f5f0c486
AJINKYA <ajinkyacodes@gmail.com>
2023-07-14 16:20:34 +0530

    text-voice Stop Voice button added

--- text-voice/script.js
+++ text-voice/script.js
@@ -1,5 +1,9 @@
 const textarea = document.getElementById('text');
 const readBtn = document.getElementById('read');
+const stopVoice = document.getElementById('stop-voice-btn');
+
+// SpeechSynthesis: speaking property
+const synth = window.speechSynthesis;
 
 // Init speech synth
 const message = new SpeechSynthesisUtterance();
@@ -19,8 +23,17 @@ function setVoice() {
   message.voice = "Google US English";
 }
 
+// Stop voice
+function cancelVoice() {
+  synth.cancel(message);
+}
+
 // Read text button
 readBtn.addEventListener('click', () => {
   setTextMessage(textarea.value);
   speakText();
+});
+
+stopVoice.addEventListener('click', () => {
+  cancelVoice();
 });
