809ba2d771d8f0103c13df7d7da6ccbca5767fd2
AJINKYA <ajinkyacodes@gmail.com>
2023-07-20 15:01:52 +0530

    exchange-rate swap functionality created

--- exchange-rate/app.js
+++ exchange-rate/app.js
@@ -23,4 +23,17 @@ function caclulate() {
     });
 }
 
+// Event listeners
+currencyEl_one.addEventListener('change', caclulate);
+amountEl_one.addEventListener('input', caclulate);
+currencyEl_two.addEventListener('change', caclulate);
+amountEl_two.addEventListener('input', caclulate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  caclulate();
+});
+
 caclulate();
