32f7232748337b8c490fd7a8f3c4a2bf93470749
AJINKYA <ajinkyacodes@gmail.com>
2023-08-11 00:51:14 +0530

    todo-list light-dark theme functionality created

--- todolist/assets/js/app.js
+++ todolist/assets/js/app.js
@@ -1,8 +1,42 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
-
+const todoIcon = document.getElementById("todo-icon");
+const developerInfoImage = document.querySelector("#developer-info img");
 const todos = JSON.parse(localStorage.getItem('todos'));
+
+//Set theme on refresh
+const currentTheme = localStorage.getItem('todo-theme');
+if(currentTheme) {
+	if(currentTheme == 'dark') {
+	document.documentElement.setAttribute('data-theme', 'dark');
+	developerInfoImage.src = "assets/images/ajinkyacodes-white.png";
+	} else if(currentTheme == 'light') {
+	document.documentElement.setAttribute('data-theme', 'light');
+	developerInfoImage.src = "assets/images/ajinkyacodes-black.png";
+	}
+} else {
+	localStorage.setItem('todo-theme', 'light');
+	document.documentElement.setAttribute('todo-theme', 'light');
+	developerInfoImage.src = "assets/images/ajinkyacodes-black.png";
+}
+
+// Light/Dark Theme Setting
+function switchTheme() {
+	const theme = localStorage.getItem("todo-theme");
+	if(theme !== null) {
+		if(theme == "light") {
+			developerInfoImage.src = "assets/images/ajinkyacodes-white.png";
+			document.documentElement.setAttribute('data-theme', 'dark');
+			localStorage.setItem('todo-theme', 'dark');
+		} else if(theme == "dark") {
+			developerInfoImage.src = "assets/images/ajinkyacodes-black.png";
+			document.documentElement.setAttribute('data-theme', 'light');
+			localStorage.setItem('todo-theme', 'light');
+		}
+	} 
+}
+
 
 if(todos) {
 	todos.forEach(todo => addTodo(todo));
@@ -59,3 +93,6 @@ function updateLS() {
 
 	localStorage.setItem('todos', JSON.stringify(todos));
 }
+
+// Event Listeners
+todoIcon.addEventListener("click", switchTheme);
